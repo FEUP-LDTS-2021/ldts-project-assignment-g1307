2a8e44df84df9cc07a1afae95c2b369b13aed33c
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-12-29 00:45:08 +0000

    Moving Behaviour composition group implemented

--- src/main/java/model/game/pieces/movingBehaviours/MovingBehaviourGroup.java
+++ src/main/java/model/game/pieces/movingBehaviours/MovingBehaviourGroup.java
@@ -1,4 +1,36 @@
 package model.game.pieces.movingBehaviours;
 
-public class MovingBehaviourGroup {
+import model.game.BoardModel;
+import model.game.Position;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MovingBehaviourGroup implements MovingBehaviour{
+    Set<MovingBehaviour> movingBehaviours;
+
+    MovingBehaviourGroup(Set<MovingBehaviour> movingBehaviours) {
+        this.movingBehaviours = movingBehaviours;
+    }
+
+    @Override
+    public Set<Position> getMoves(BoardModel b, Position objectPosition) {
+        Set<Position> onBoardMoves = new HashSet<>();
+        for (MovingBehaviour movingBehaviour: movingBehaviours) {
+            onBoardMoves.addAll(movingBehaviour.getMoves(b,objectPosition));
+        }
+        return onBoardMoves;
+    }
+
+    public void add(MovingBehaviour movingBehaviour) {
+        movingBehaviours.add(movingBehaviour);
+    }
+
+    public void remove(MovingBehaviour movingBehaviour) {
+        movingBehaviours.remove(movingBehaviour);
+    }
+
+    public Set<MovingBehaviour> getMovingBehaviours(){
+        return movingBehaviours;
+    }
 }
