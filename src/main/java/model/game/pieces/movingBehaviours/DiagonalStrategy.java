4fd6ba49354ec3c7204cd0e8a1a9e940afd25522
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2021-12-29 14:29:14 +0000

    Implemented DiagonalStrategy and SideStrategy

--- src/main/java/model/game/pieces/movingBehaviours/DiagonalStrategy.java
+++ src/main/java/model/game/pieces/movingBehaviours/DiagonalStrategy.java
@@ -9,6 +9,52 @@ import java.util.Set;
 public class DiagonalStrategy implements MovingBehaviour{
     @Override
     public Set<Position> getMoves(BoardModel b, Position objectPosition) {
-        return new HashSet<>();
+        Set<Position> possibleMoves = new HashSet<>();
+
+        int x = objectPosition.getCol() - 1;
+        int y = objectPosition.getRow() - 1;
+
+        while (b.positionInBoard(new Position(y, x))){
+            Position movePosition = new Position(y, x);
+            possibleMoves.add(movePosition);
+
+            x--;
+            y--;
+        }
+
+        x = objectPosition.getCol() - 1;
+        y = objectPosition.getRow() + 1;
+
+        while (b.positionInBoard(new Position(y, x))){
+            Position movePosition = new Position(y, x);
+            possibleMoves.add(movePosition);
+
+            x--;
+            y++;
+        }
+
+        x = objectPosition.getCol() + 1;
+        y = objectPosition.getRow() + 1;
+
+        while (b.positionInBoard(new Position(y, x))){
+            Position movePosition = new Position(y, x);
+            possibleMoves.add(movePosition);
+
+            x++;
+            y++;
+        }
+
+        x = objectPosition.getCol() + 1;
+        y = objectPosition.getRow() - 1;
+
+        while (b.positionInBoard(new Position(y, x))){
+            Position movePosition = new Position(y, x);
+            possibleMoves.add(movePosition);
+
+            x++;
+            y--;
+        }
+
+        return possibleMoves;
     }
 }
