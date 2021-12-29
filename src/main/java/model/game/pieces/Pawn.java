d4111082f3656f71ec83445c2a8861bc235bdbce
Valkajjik <lcsferreiradesousa@hotmail.com>
2021-12-29 18:57:39 +0000

    replaced MovingBehaviour in pawn with TwoAndOneStrategy

--- src/main/java/model/game/pieces/Pawn.java
+++ src/main/java/model/game/pieces/Pawn.java
@@ -2,10 +2,11 @@ package model.game.pieces;
 
 import model.game.Position;
 import model.game.pieces.movingBehaviours.MovingBehaviour;
+import model.game.pieces.movingBehaviours.TwoAndOneStrategy;
 
 public class Pawn extends Piece{  // TODO: See what is the figure for this piece in the font
     boolean hasMoved;
-    public Pawn(COLOR color, char figure, Position position, MovingBehaviour movingBehaviour) {
-        super(color, 'o', position, movingBehaviour); // TODO: Moving behaviour for Pawn is yet to be done
+    public Pawn(COLOR color, char figure, Position position, TwoAndOneStrategy movingBehaviour) {
+        super(color, 'o', position, movingBehaviour);
     }
 }
