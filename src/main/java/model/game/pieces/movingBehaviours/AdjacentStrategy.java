1dc28194de094ce187b59788036c6da4786ac3ba
Valkajjik <lcsferreiradesousa@hotmail.com>
2021-12-29 14:26:59 +0000

    "AdjacentStrategy and LStrategy"

--- src/main/java/model/game/pieces/movingBehaviours/AdjacentStrategy.java
+++ src/main/java/model/game/pieces/movingBehaviours/AdjacentStrategy.java
@@ -3,11 +3,36 @@ package model.game.pieces.movingBehaviours;
 import model.game.BoardModel;
 import model.game.Position;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AdjacentStrategy implements MovingBehaviour{
     @Override
     public Set<Position> getMoves(BoardModel b, Position objectPosition) {
-        return null;
+        Set<Position> res = new HashSet<>();
+
+        int row = objectPosition.getRow();
+        int col = objectPosition.getCol();
+
+        List<Position> alist = Arrays.asList(new Position(row -1, col -1), new Position(row -1, col), new Position(row-1, col+1),
+                                new Position(row, col-1), new Position(row, col+1), new Position(row+1, col-1), new Position(row+1, col+1),
+                                new Position(row+1, col));
+
+        for (Position position : alist){
+            if(isInBounds(b, position)) res.add(position);
+        }
+        return res;
+    }
+
+    private boolean isInBounds(BoardModel b, Position position){
+        int rows = b.getRows();
+        int columns = b.getColumns();
+
+        if(position.getRow() > 0 && position.getRow() <= rows){
+            return position.getCol() > 0 && position.getCol() <= columns;
+        }
+        return false;
     }
 }
