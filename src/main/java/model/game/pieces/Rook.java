2a531f0dd2f58dfafd7ff19f3e7b41b742fc27e5
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-12-30 17:22:20 +0000

    King,Pawn and Rook override of moveToPosition

--- src/main/java/model/game/pieces/Rook.java
+++ src/main/java/model/game/pieces/Rook.java
@@ -3,9 +3,20 @@ package model.game.pieces;
 import model.game.Position;
 import model.game.pieces.movingBehaviours.SideStrategy;
 
-public class Rook extends Piece{// TODO: See what is the figure for this piece in the font
+public class Rook extends Piece{
     boolean hasMoved;
     public Rook(COLOR color, Position position) {
         super(color, 't', position, new SideStrategy());
+        hasMoved = false;
+    }
+
+    public boolean isMoved() {
+        return hasMoved;
+    }
+
+    @Override
+    public void moveToPosition(Position position) {
+        super.moveToPosition(position);
+        hasMoved = true;
     }
 }
