9bc8d654f15f2c5fe93d3ae0460ba50b30866131
Valkajjik <lcsferreiradesousa@hotmail.com>
2022-01-01 22:37:58 +0000

    WhiteSquares in SquareBoard implemented

--- src/main/java/model/game/board/SquareBoard.java
+++ src/main/java/model/game/board/SquareBoard.java
@@ -1,15 +1,18 @@
-package model.game.board;
+package model.game;
 
-import model.game.Position;
-import model.game.board.BoardModel;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SquareBoard implements BoardModel {
     private final int rows;
     private final int columns;
 
+    private final Set<Position> whiteSquares;
+
     public SquareBoard(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
+        whiteSquares = createWhiteSquares();
     }
 
     public int getRows() {
@@ -23,5 +26,19 @@ public class SquareBoard implements BoardModel {
     @Override
     public boolean positionInBoard(Position position) {
         return position.getCol() > 0 && position.getCol() <= columns && position.getRow() > 0 && position.getRow() <= rows;
+    }
+
+    private Set<Position> createWhiteSquares(){
+        Set<Position> res = new HashSet<>();
+        for(int row = 1; row <= 32; row++){
+            for(int col = 1; col <= 32; col++){
+                if((row+col)%2 == 0) res.add(new Position(row, col));
+            }
+        }
+        return res;
+    }
+
+    public Set<Position> getWhiteSquares() {
+        return whiteSquares;
     }
 }
