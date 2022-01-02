629b2d30a23b8a09ab5ce529a15e19183307a329
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-01-02 03:35:24 +0000

    refractoring of squareBoard

--- src/main/java/model/game/board/SquareBoard.java
+++ src/main/java/model/game/board/SquareBoard.java
@@ -1,5 +1,8 @@
-package model.game;
+package model.game.board;
 
+import model.game.Position;
+
+import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,12 +10,37 @@ public class SquareBoard implements BoardModel {
     private final int rows;
     private final int columns;
 
-    private final Set<Position> whiteSquares;
+    private final Set<BoardCase> boardCases;
+    private final Color blackCaseColor;
+    private final Color whiteCaseColor;
 
-    public SquareBoard(int rows, int columns) {
-        this.rows = rows;
-        this.columns = columns;
-        whiteSquares = createWhiteSquares();
+    public SquareBoard(int rowsAndCols, Color whiteCaseColor, Color blackCaseColor) { // maybe add a config that has all the information needed
+        this.rows = rowsAndCols;
+        this.columns = rowsAndCols;
+        this.whiteCaseColor = whiteCaseColor;
+        this.blackCaseColor = blackCaseColor;
+        boardCases = createBoardCases();
+    }
+
+    public SquareBoard(int rowsAndCols) {
+        this.rows = rowsAndCols;
+        this.columns = rowsAndCols;
+        this.whiteCaseColor = Color.lightGray;
+        this.blackCaseColor = Color.green;
+        boardCases = createBoardCases();
+    }
+
+    private Set<BoardCase> createBoardCases() {
+        Set<BoardCase> boardCases = new HashSet<>();
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= columns; col++) {
+                if((row+col)%2 == 0)
+                    boardCases.add(new BoardCase(whiteCaseColor, new Position(row,col)));
+                else
+                    boardCases.add(new BoardCase(blackCaseColor, new Position(row,col)));
+            }
+        }
+        return boardCases;
     }
 
     public int getRows() {
@@ -28,17 +56,8 @@ public class SquareBoard implements BoardModel {
         return position.getCol() > 0 && position.getCol() <= columns && position.getRow() > 0 && position.getRow() <= rows;
     }
 
-    private Set<Position> createWhiteSquares(){
-        Set<Position> res = new HashSet<>();
-        for(int row = 1; row <= 32; row++){
-            for(int col = 1; col <= 32; col++){
-                if((row+col)%2 == 0) res.add(new Position(row, col));
-            }
-        }
-        return res;
-    }
-
-    public Set<Position> getWhiteSquares() {
-        return whiteSquares;
+    @Override
+    public Set<BoardCase> getCases() {
+        return boardCases;
     }
 }
