d33a4ad4d4b0c385335755548899e9b0c99dac0d
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-01-03 02:26:36 +0000

    Implemented GameCursor

--- src/main/java/model/game/GameCursor.java
+++ src/main/java/model/game/GameCursor.java
@@ -25,18 +25,26 @@ public class GameCursor {
     }
 
     public void moveUp() {
-
+        currentPosition = currentPosition.sub(new Position(1,0));
+        if (!boardModel.positionInBoard(currentPosition))
+            currentPosition = currentPosition.add(new Position(boardModel.getRows(),0));
     }
 
     public void moveDown() {
-
+        currentPosition = currentPosition.add(new Position(1, 0));
+        if (!boardModel.positionInBoard(currentPosition))
+            currentPosition = currentPosition.sub(new Position(boardModel.getRows(),0));
     }
 
     public void moveLeft() {
-
+        currentPosition = currentPosition.sub(new Position(0, 1));
+        if (!boardModel.positionInBoard(currentPosition))
+            currentPosition = currentPosition.add(new Position(0,boardModel.getColumns()));
     }
 
     public void moveRight() {
-
+        currentPosition = currentPosition.add(new Position(0, 1));
+        if (!boardModel.positionInBoard(currentPosition))
+            currentPosition = currentPosition.sub(new Position(0,boardModel.getColumns()));
     }
 }
