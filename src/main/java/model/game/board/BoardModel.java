964777598c2c659cc2d4b60d02d8ffa3cc2a7159
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-01-03 01:14:32 +0000

    Made minor changes to BoardModel

--- src/main/java/model/game/board/BoardModel.java
+++ src/main/java/model/game/board/BoardModel.java
@@ -7,5 +7,7 @@ import java.util.Set;
 
 public interface BoardModel extends Model {
     boolean positionInBoard(Position position);
+    public int getRows();
+    public int getColumns();
     Set<BoardCase> getCases();
 }
