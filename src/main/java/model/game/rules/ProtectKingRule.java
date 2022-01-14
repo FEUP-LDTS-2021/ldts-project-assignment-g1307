b72cbde1745894cead1fd551d7d8d678b2f1be80
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-01-14 23:53:13 +0000

    Implemented ProtectKingRule

--- src/main/java/model/game/rules/ProtectKingRule.java
+++ src/main/java/model/game/rules/ProtectKingRule.java
@@ -1,8 +1,11 @@
 package model.game.rules;
 
+import jdk.jshell.spi.ExecutionControl;
 import model.game.GameModel;
 import model.game.move.Move;
+import model.game.pieces.King;
 import model.game.pieces.Piece;
+import model.game.player.Player;
 
 import java.util.Set;
 
@@ -15,6 +18,25 @@ public class ProtectKingRule implements Rule{
 
     @Override
     public void obyRule(Set<Move> movesToFilter, Piece piece) {
+        King king = null;
 
+        for(Piece p : gameModel.getPiecesInGame()){
+            if (p instanceof King && p.getColor() == piece.getColor()) {
+                king = (King) p;
+                break;
+            }
+        }
+
+        gameModel.setCheck();
+        if (king.inCheck())
+            return;
+
+        gameModel.getPiecesInGame().remove(piece);
+        gameModel.setCheck();
+
+        if (king.inCheck()){
+            gameModel.getPiecesInGame().add(piece);
+            movesToFilter.clear();
+        }
     }
 }
