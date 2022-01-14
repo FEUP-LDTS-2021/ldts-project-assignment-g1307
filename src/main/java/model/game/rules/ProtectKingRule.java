248c7fb4c854cc6464807b35b38e25f3470dc07c
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-01-14 17:03:54 +0000

    Interface for ProtectingKingRule

---
+++ src/main/java/model/game/rules/ProtectKingRule.java
@@ -0,0 +1,13 @@
+package model.game.rules;
+
+import model.game.move.Move;
+import model.game.pieces.Piece;
+
+import java.util.Set;
+
+public class ProtectKingRule implements Rule{
+    @Override
+    public void obyRule(Set<Move> movesToFilter, Piece piece) {
+
+    }
+}
