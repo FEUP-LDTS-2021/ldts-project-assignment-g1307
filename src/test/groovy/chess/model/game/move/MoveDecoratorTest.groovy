aa573d6f980925b920a0a2584b3b7b6ae91f19a8
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2022-01-19 23:25:20 +0000

    improved move tests

--- src/test/groovy/chess/model/game/move/MoveDecoratorTest.groovy
+++ src/test/groovy/chess/model/game/move/MoveDecoratorTest.groovy
@@ -1,16 +1,21 @@
 package chess.model.game.move
 
+import chess.model.game.Position
 import spock.lang.Specification
 
 class MoveDecoratorTest extends Specification {
     def "Execute"() {
         given:
         def s = Mock(Move)
-        def spy = Spy(MoveDecorator)
+        s.getPosition() >> new Position(0,0)
+        def decor = new MoveDecorator(s)
+        def spy = Spy(decor)
         spy.moveWrappee = s
         when:
         spy.execute()
         then:
         1 * spy.moveWrappee.execute()
+        expect:
+        decor.getPosition() != null
     }
 }
