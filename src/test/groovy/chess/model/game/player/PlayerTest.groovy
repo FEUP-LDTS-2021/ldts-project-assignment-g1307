0941f6e1b85c92d2054f8a13fa7d0a3815120ac7
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2022-01-19 23:59:22 +0000

    improved player test

--- src/test/groovy/chess/model/game/player/PlayerTest.groovy
+++ src/test/groovy/chess/model/game/player/PlayerTest.groovy
@@ -6,14 +6,22 @@ import spock.lang.Specification
 
 class PlayerTest extends Specification {
     def "SetTurn"() {
+        given:
         def clock = Mock(ClockModel)
         def player = new Player(clock, Piece.COLOR.BLACK)
 
         when:
         player.setTurn(true)
-        player.setTurn(false)
         then:
         1 * clock.resume()
+
+        when:
+        player.setTurn(false)
+        then:
         1 * clock.pause()
+
+        expect:
+        player.getClock() != null
+        player.getColor() != null
     }
 }
