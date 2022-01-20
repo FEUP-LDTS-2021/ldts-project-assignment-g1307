6c98c6ac42211697b0a6e8442dcda0fc35dc8711
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-01-20 19:38:40 +0000

    more Tests for Menu View

--- src/test/groovy/chess/view/menu/MenuViewTest.groovy
+++ src/test/groovy/chess/view/menu/MenuViewTest.groovy
@@ -1,5 +1,6 @@
 package chess.view.menu
 
+import com.googlecode.lanterna.TerminalPosition
 import com.googlecode.lanterna.graphics.TextGraphics
 import com.googlecode.lanterna.screen.Screen
 import chess.model.menu.MenuModel
@@ -22,5 +23,13 @@ class MenuViewTest extends Specification {
         1 * spy.clear()
         1 * spy.drawOption()
         1 * spy.drawTitle()
+        _ * screen.refresh()
+        1 * graphics.putString(new TerminalPosition(-16,5), 'CLASSICAL')
+        _ * graphics.drawRectangle()
+        1 * graphics.putString(0, 3, _)
+        spy.getScreen() == screen
+        spy.getModel() == model
+        spy.getGraphics() == graphics
+
     }
 }
