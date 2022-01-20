d951315fd619316818703202dced0efa3903f0e1
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-01-20 19:17:53 +0000

    Improved Sound class interface

--- src/main/java/model/game/sound/Sound.java
+++ src/main/java/model/game/sound/Sound.java
@@ -2,20 +2,36 @@ package model.game.sound;
 
 import jdk.jshell.spi.ExecutionControl;
 import model.game.GameModel;
+import model.game.move.CapturingMove;
 import model.game.move.Move;
+
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import java.io.File;
+
 
 public class Sound {
     private final GameModel gameModel;
+    private Clip clip;
 
     public Sound(GameModel gameModel){
         this.gameModel = gameModel;
+        this.clip = null;
     }
 
     public void selectSound(Move move) throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("N implementado");
     }
 
-    public void playSound(String filePath) throws ExecutionControl.NotImplementedException {
+    public Clip loadSound(String filePath) throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("N implementado");
     }
+
+    public void playSound(Move move) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("N implementado");
+    }
+
 }
