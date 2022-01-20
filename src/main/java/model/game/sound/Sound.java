600de27ad850de7ba828a549d61bd592e61ee8c6
Vitor Cavaleiro <vcavaleiro75@gmail.com>
2022-01-20 19:24:04 +0000

    Implemented Sound class

--- src/main/java/model/game/sound/Sound.java
+++ src/main/java/model/game/sound/Sound.java
@@ -1,6 +1,5 @@
 package model.game.sound;
 
-import jdk.jshell.spi.ExecutionControl;
 import model.game.GameModel;
 import model.game.move.CapturingMove;
 import model.game.move.Move;
@@ -22,16 +21,33 @@ public class Sound {
         this.clip = null;
     }
 
-    public void selectSound(Move move) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("N implementado");
+    public void selectSound(Move move) {
+        if (gameModel.gameEnded())
+            clip = loadSound("src/main/resources/gameover.wav");
+        else if (move instanceof CapturingMove)
+            clip = loadSound("src/main/resources/capture.wav");
+        else
+            clip = loadSound("src/main/resources/move.wav");
     }
 
-    public Clip loadSound(String filePath) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("N implementado");
+    public Clip loadSound(String filePath) throws NullPointerException{
+        try {
+            AudioInputStream audioInput = AudioSystem.getAudioInputStream(new File(filePath));
+            Clip soundClip = AudioSystem.getClip();
+            soundClip.open(audioInput);
+            FloatControl gainControl = (FloatControl) soundClip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-25.0f);
+            return soundClip;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public void playSound(Move move) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("N implementado");
+    public void playSound(Move move){
+        selectSound(move);
+        clip.setMicrosecondPosition(0);
+        clip.start();
     }
 
 }
